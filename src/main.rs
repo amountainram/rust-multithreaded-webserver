190e0d7834e18dcdf7c3b8895c13ec68233df854
Umberto Toniolo <amountainram@gmail.com>
2022-11-09 17:38:07 +0100

    basic response next: request validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let _: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let content = fs::read_to_string("hello.html").unwrap();
+    let length = content.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
 }
